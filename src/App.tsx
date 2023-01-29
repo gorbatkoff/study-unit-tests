33afccb8a4d36be0013029b922595eaf42737218
gorbatkoff <gorbatkovartyom@gmail.com>
2023-01-30 00:51:02 +0300

    Change smth

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ type Props = {}
 
 // eslint-disable-next-line no-empty-pattern
 function App({}: Props) {
-    return (
-        <div>
-            hello world
-        </div>
-    )
+  return (
+    <div>
+      <h1>Hello world!</h1>
+
+      <h3>Welcome to my new project. I code it in WebStorm</h3>
+    </div>
+  )
 }
 
 export default App
